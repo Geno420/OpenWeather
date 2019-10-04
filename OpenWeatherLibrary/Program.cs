b1a204b20b5cade0b038ffffe3ac6615bb767a1b
Geno420 <56066010+Geno420@users.noreply.github.com>
2019-10-04 11:45:00 +0200

    update

---
+++ OpenWeatherLibrary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using OpenWeatherLibrary;
+
+namespace OpenWeatherLibrary
+{
+    class Program
+    {
+       
+        
+            
+
+            
+            static async Task Main(string[] args)
+            { 
+                string weatherString = await WeatherGenerator.GetWeatherDataAsync();
+                Console.WriteLine(weatherString);
+
+
+            }
+
+        }
+
+    }
+
---
+++ OpenWeatherLibrary/WeatherGenerator.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using OpenWeatherLibrary;
+using System.Linq;
+using static OpenWeatherLibrary.WeatherData;
+
+namespace OpenWeatherLibrary
+{
+    public class WeatherGenerator
+    {
+        public static async Task<string> GetWeatherDataAsync()
+        {
+            HttpClient httpClient = new HttpClient();
+
+            string dj = await httpClient.GetStringAsync("http://api.openweathermap.org/data/2.5/weather?q=Cape%20Town&APPID=cfabe0e4e34a2060a19f2daea46f0fb4");
+
+            WeatherDate weatherDate = JsonConvert.DeserializeObject<WeatherDate>(dj);
+
+            string returnString = string.Empty;
+
+            for (int index = 0; index < weatherDate.weather.Count; index++)
+            {
+                returnString = weatherDate.weather[index].main;
+            }
+
+
+            return returnString;
+
+
+        }
+    }
+}
